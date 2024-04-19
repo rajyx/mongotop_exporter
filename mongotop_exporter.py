81398b7c3b9f8e2906690807aaa76e50cbf38d07
ematveev <rajyxmagyx@gmail.com>
2024-04-19 07:39:47 +0300

    merge collections by name

--- mongotop_exporter.py
+++ mongotop_exporter.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import time
 import argparse
+import pandas as pd
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--host")
@@ -18,16 +19,28 @@ client = MongoClient(
 admin_db = client.admin
 previous_top = admin_db.command("top")["totals"]
 previous_top.pop('note')
+previous_top_df = pd.DataFrame.from_dict(previous_top, orient="index")[['total']]
 
 time.sleep(5)
 
 next_top = admin_db.command("top")["totals"]
 next_top.pop('note')
+next_top_df = pd.DataFrame.from_dict(next_top, orient="index")[['total']]
 
-for collection in previous_top:
-    print(f"{collection} {previous_top[collection]['total']}")
+print(previous_top_df.to_string())
 
 print("Try to get next top")
 
-for coll in next_top:
-    print(f"{coll} {next_top[coll]['total']}")
+print(next_top_df.to_string())
+
+print("Let's try to merge frames")
+
+merged_df = next_top_df.merge(
+    previous_top_df,
+    how="inner",
+    left_index=True,
+    right_index=True,
+    suffixes=["_next", "_previous"]
+)
+
+print(merged_df.to_string())
