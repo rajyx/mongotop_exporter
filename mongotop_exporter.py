9bcfede337f16826164fa913171ff40553e2c335
ematveev <rajyxmagyx@gmail.com>
2024-04-22 07:50:06 +0300

    try to use metrics list instead of single value

--- mongotop_exporter.py
+++ mongotop_exporter.py
@@ -20,13 +20,14 @@ client = MongoClient(
 admin_db = client.admin
 previous_top = admin_db.command("top")["totals"]
 previous_top.pop('note')
-previous_top_df = pd.DataFrame.from_dict(previous_top, orient="index")[['total']]
+metrics = ['total']
+previous_top_df = pd.DataFrame.from_dict(previous_top, orient="index")[metrics]
 
 time.sleep(5)
 
 next_top = admin_db.command("top")["totals"]
 next_top.pop('note')
-next_top_df = pd.DataFrame.from_dict(next_top, orient="index")[['total']]
+next_top_df = pd.DataFrame.from_dict(next_top, orient="index")[metrics]
 
 print(previous_top_df.to_string())
 
