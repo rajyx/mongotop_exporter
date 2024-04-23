b614d5996fca05b08199f2f7d898151b7515b8dc
ematveev <rajyxmagyx@gmail.com>
2024-04-23 06:00:03 +0300

    remove debug message

--- mongotop_exporter.py
+++ mongotop_exporter.py
@@ -29,14 +29,6 @@ next_top = admin_db.command("top")["totals"]
 next_top.pop('note')
 next_top_df = pd.DataFrame.from_dict(next_top, orient="index")[metrics]
 
-print(previous_top_df.to_string())
-
-print("Try to get next top")
-
-print(next_top_df.to_string())
-
-print("Let's try to merge frames")
-
 merged_df = next_top_df.merge(
     previous_top_df,
     how="inner",
