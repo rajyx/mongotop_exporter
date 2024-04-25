60aae2015c822afc1f46f0c8b5546278d7bb7b83
ematveev <rajyxmagyx@gmail.com>
2024-04-25 07:56:18 +0300

    rename utility main files

--- mongotop_exporter.py
+++ mongotop_exporter.py
@@ -1,58 +1,35 @@
-from abc import ABC, abstractmethod
-from global_functions.df_operations import (
-    get_top_df,
-    add_metrics_delta,
-    add_all_metrics_prometheus_output,
-    get_all_metrics_prometheus_output
+import argparse
+
+from flask import Flask, Response
+from pymongo import MongoClient
+
+from service import MongoTopPrometheusExporterService
+
+app = Flask(__name__)
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--host")
+arg_parser.add_argument("--port")
+arg_parser.add_argument("--username")
+arg_parser.add_argument("--password")
+args = arg_parser.parse_args()
+
+client = MongoClient(
+    host=args.host,
+    port=int(args.port),
+    username=args.username,
+    password=args.password
+)
+
+top_exporter = MongoTopPrometheusExporterService(
+    db=client.admin,
+    metrics=['total']
 )
 
 
-class MongoTopExporter(ABC):
-    def __init__(self, db, metrics):
-        self.__db = db
-        self.__metrics = metrics
-        self.__prev_top = None
-
-    @property
-    def db(self):
-        return self.__db
-
-    @property
-    def metrics(self):
-        return self.__metrics
-
-    @property
-    def prev_top(self):
-        return self.__prev_top
-
-    @prev_top.setter
-    def prev_top(self, prev_top):
-        self.__prev_top = prev_top
-
-    @abstractmethod
-    def get_top_output(self):
-        pass
+@app.route("/metrics")
+def metrics():
+    return Response(top_exporter.get_top_output(), mimetype="text/plain")
 
 
-class MongoTopPrometheusExporter(MongoTopExporter):
-    def get_top_output(self):
-        if self.prev_top is None:
-            self.prev_top = get_top_df(
-                self.db,
-                self.metrics
-            )
-        next_top = get_top_df(
-            self.db,
-            self.metrics
-        )
-        merged_top = next_top.merge(
-            self.prev_top,
-            how="inner",
-            left_index=True,
-            right_index=True,
-            suffixes=["_next", "_previous"]
-        )
-        self.prev_top = next_top
-        add_metrics_delta(merged_top, self.metrics)
-        add_all_metrics_prometheus_output(merged_top, self.metrics)
-        return get_all_metrics_prometheus_output(merged_top, self.metrics)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ service.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from global_functions.df_operations import (
+    get_top_df,
+    add_metrics_delta,
+    add_all_metrics_prometheus_output,
+    get_all_metrics_prometheus_output
+)
+
+
+class MongoTopExporterService(ABC):
+    def __init__(self, db, metrics):
+        self.__db = db
+        self.__metrics = metrics
+        self.__prev_top = None
+
+    @property
+    def db(self):
+        return self.__db
+
+    @property
+    def metrics(self):
+        return self.__metrics
+
+    @property
+    def prev_top(self):
+        return self.__prev_top
+
+    @prev_top.setter
+    def prev_top(self, prev_top):
+        self.__prev_top = prev_top
+
+    @abstractmethod
+    def get_top_output(self):
+        pass
+
+
+class MongoTopPrometheusExporterService(MongoTopExporterService):
+    def get_top_output(self):
+        if self.prev_top is None:
+            self.prev_top = get_top_df(
+                self.db,
+                self.metrics
+            )
+        next_top = get_top_df(
+            self.db,
+            self.metrics
+        )
+        merged_top = next_top.merge(
+            self.prev_top,
+            how="inner",
+            left_index=True,
+            right_index=True,
+            suffixes=["_next", "_previous"]
+        )
+        self.prev_top = next_top
+        add_metrics_delta(merged_top, self.metrics)
+        add_all_metrics_prometheus_output(merged_top, self.metrics)
+        return get_all_metrics_prometheus_output(merged_top, self.metrics)
