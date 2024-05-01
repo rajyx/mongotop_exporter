78c8f3c380fc650f681dade44474c2a6659cf3fd
ematveev <rajyxmagyx@gmail.com>
2024-05-01 15:17:48 +0300

    add method to check metric delta calculation is correct for each input metric

--- test_common_df_functions.py
+++ test_common_df_functions.py
@@ -3,7 +3,8 @@ import unittest
 
 from test_utils.setup import prepare_merged_df
 from common.df_functions import (
-    add_metric_delta
+    add_metric_delta,
+    add_metrics_delta
 )
 from common.global_vars import metrics
 
@@ -17,6 +18,21 @@ class TestCommonDFFunctions(unittest.TestCase):
         df = self.merged_top
         metric = metrics[random.randint(0, len(metrics) - 1)]
         add_metric_delta(df, metric)
+        self.__check_dataframe_metric_delta_column_exists_and_has_correct_value(
+            df,
+            metric
+        )
+
+    def test_add_metrics_delta_creates_delta_column_for_each_input_metric(self):
+        df = self.merged_top
+        add_metrics_delta(df, metrics)
+        for metric in metrics:
+            self.__check_dataframe_metric_delta_column_exists_and_has_correct_value(
+                df,
+                metric
+            )
+
+    def __check_dataframe_metric_delta_column_exists_and_has_correct_value(self, df, metric):
         collection_row = df.loc[self.collection_path]
         next_metric_value = collection_row[f"{metric}_next"]
         prev_metric_value = collection_row[f"{metric}_previous"]
