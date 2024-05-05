ade7a7527065378aa0e5f2ab61614242e3f9bb23
ematveev <rajyxmagyx@gmail.com>
2024-05-05 07:17:29 +0300

    add method to prepare multi collection merged dataframe in setup.py

--- test_utils/setup.py
+++ test_utils/setup.py
@@ -7,12 +7,31 @@ from common.global_vars import metrics
 
 def prepare_merged_df(collection_path):
     df_dict = {
-        collection_path: {}
+        collection_path: prepare_collection_dict()
     }
+    merged_top = pd.DataFrame.from_dict(
+        df_dict,
+        orient="index"
+    )
+    return merged_top
+
+
+def prepare_multi_collection_merged_df(collection_quantity):
+    df_dict = {}
+    for number in range(1, collection_quantity + 1):
+        df_dict[f"db.collection_{number}"] = prepare_collection_dict()
+    return pd.DataFrame.from_dict(
+        df_dict,
+        orient="index"
+    )
+
+
+def prepare_collection_dict():
+    collection_dict = {}
     for metric in metrics:
         metric_time = random.randint(0, 100)
         metric_count = random.randint(0, 100)
-        df_dict[collection_path].update(
+        collection_dict.update(
             {
                 f"{metric}_previous": {
                     "time": metric_time,
@@ -24,8 +43,4 @@ def prepare_merged_df(collection_path):
                 }
             }
         )
-    merged_top = pd.DataFrame.from_dict(
-        df_dict,
-        orient="index"
-    )
-    return merged_top
+    return collection_dict
